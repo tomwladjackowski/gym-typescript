7183caf198da94d56dbb0ad38376d803cd46f361
tomwladjackowski <125567834+tomwladjackowski@users.noreply.github.com>
2024-02-06 19:14:07 +0100

    initial commmit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/scense/navbar"
+function App() {
+
+  return (
+    <div className='app'>
+      app
+    </div>
+  )
+}
+
+export default App
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { Bars3Icon, XMarkIcon} from "@heroicons/react/16/solid";
+import Logo from "@/assets/Logo.png";
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>Navbar</div>
+  )
+}
